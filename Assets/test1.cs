303240f8af964131fc25e3583d2c4c44b3e13c2c
jjh <78027838+beautifulmelon@users.noreply.github.com>
2025-05-30 15:15:08 +0900

    .
    .

---
+++ Assets/test1.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class test1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Debug.Log(transform.rotation.y); 
+    }
+}
