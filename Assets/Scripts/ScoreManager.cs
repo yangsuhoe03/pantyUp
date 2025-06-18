9c7b7ea480f8fa12a3efd244ad8102c831d50742
jjh <78027838+beautifulmelon@users.noreply.github.com>
2025-06-19 00:54:22 +0900

    .
    .

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -8,9 +8,9 @@ public class ScoreManager : MonoBehaviour
 {
     private Dictionary<string, (int score, string nickname)> playerScores = new Dictionary<string, (int score, string nickname)>();
     public TextMeshProUGUI scoreText; // UI용 TextMeshPro
-    public GameObject nicknamePanel; // 닉네임 설정 UI 패널
     public TMP_InputField nicknameInput; // 닉네임 입력 필드
     public TextMeshProUGUI randomNicknameText; // 랜덤 닉네임 표시 텍스트
+    public string userName = null;
 
     private void Start()
     {
@@ -18,9 +18,18 @@ public class ScoreManager : MonoBehaviour
         {
             Debug.LogWarning("Score TextMeshProUGUI가 할당되지 않았습니다.");
         }
-        GenerateRandomNickname();
         // 게임 시작 시 마우스 커서 표시
-        SetCursorState(false);
+        //SetCursorState(false);
+        if (userName == "")
+        {
+            userName = GenerateRandomNickname();
+            GameObject socketManager = GameObject.Find("SocketManager");
+            if (socketManager != null)
+            {
+                socketManager.GetComponent<SocketManager>().SetPlayerNickname(userName);
+            }
+        }
+        nicknameInput.text = userName;
     }
 
     public void SetCursorState(bool locked)
@@ -54,9 +63,13 @@ public class ScoreManager : MonoBehaviour
             string nickname = nicknameInput.text.Trim();
             if (string.IsNullOrEmpty(nickname))
             {
-                nickname = GenerateRandomNickname();
+                nickname = userName;
             }
-            
+            else
+            {
+                userName = nickname;
+            }
+
             // SocketManager에 닉네임 전달
             GameObject socketManager = GameObject.Find("SocketManager");
             if (socketManager != null)
@@ -64,14 +77,16 @@ public class ScoreManager : MonoBehaviour
                 socketManager.GetComponent<SocketManager>().SetPlayerNickname(nickname);
             }
 
+            /*
             // UI 패널 비활성화
             if (nicknamePanel != null)
             {
                 nicknamePanel.SetActive(false);
             }
+            */
 
             // 닉네임 설정이 완료되면 마우스 커서를 잠금
-            SetCursorState(true);
+            //SetCursorState(true);
         }
     }
 
@@ -89,7 +104,7 @@ public class ScoreManager : MonoBehaviour
                 string playerId = parts[0];
                 string nickname = parts[1];
                 int score = int.Parse(parts[2]);
-                
+
                 playerScores[playerId] = (score, nickname);
             }
         }
@@ -102,7 +117,7 @@ public class ScoreManager : MonoBehaviour
         if (scoreText != null)
         {
             string displayText = "Score:\n";
-            
+
             // 점수를 기준으로 내림차순 정렬
             var sortedScores = playerScores
                 .OrderByDescending(x => x.Value.score)
@@ -116,4 +131,4 @@ public class ScoreManager : MonoBehaviour
             scoreText.text = displayText;
         }
     }
-} 
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,96 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    
+    public bool gameStarted = false;
+
+    public GameObject pausePanel;
+    public GameObject customPanel;
+
+    public TextMeshProUGUI usernameText;
+
+    public Button customizeButton;
+
+    public ScoreManager scoreManager;
+    public CameraManager cameraManager;
+
+    void Start()
+    {
+        if (pausePanel != null)
+        {
+            pausePanel.GetComponent<Button>().onClick.AddListener(ResumeGame);
+        }
+        if (customPanel != null)
+        {
+            customPanel.GetComponent<Button>().onClick.AddListener(CloseCustomPanel);
+        }
+        if (customizeButton != null)
+        {
+            customizeButton.onClick.AddListener(CustomPanel);
+        }
+        if (scoreManager == null)
+        {
+            scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+        }
+        cameraManager = GetComponent<CameraManager>();
+        usernameText.text = scoreManager.userName;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !pausePanel.activeSelf)
+        {
+            PauseGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && customPanel.activeSelf)
+        {
+            CloseCustomPanel();
+        }
+    }
+
+    public void PauseGame()
+    {
+        if (!gameStarted)
+        {
+            cameraManager.SwitchCamera(0); //일단은 메인으로 돌리고 나중에 조건 나오면 그때 수정하기로
+        }
+        pausePanel.SetActive(true);
+        SetCursorState(false);
+        usernameText.text = scoreManager.userName;
+    }
+
+    public void CustomPanel()
+    {
+        customPanel.SetActive(true);
+        scoreManager.nicknameInput.text = scoreManager.userName;
+    }
+
+    public void CloseCustomPanel()
+    {
+        customPanel.SetActive(false);
+        usernameText.text = scoreManager.userName;
+    }
+
+    public void ResumeGame()
+    {
+        if(gameStarted)
+        {
+            cameraManager.SwitchCamera(0);
+        }
+        else
+        {
+            cameraManager.SwitchCamera(0); //일단은 메인카메라로 돌아가게 함 게임 시작여부가 나오면 그 때 바꾸도록
+        }
+        pausePanel.SetActive(false);
+        SetCursorState(true);
+    }
+
+    public void SetCursorState(bool locked)
+    {
+        Cursor.visible = !locked;
+        Cursor.lockState = locked ? CursorLockMode.Locked : CursorLockMode.None;
+    }
 }
